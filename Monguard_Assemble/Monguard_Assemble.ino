bfe0583bd30342cf0b68287c0ea16f56f443e03f
Jisang Yun <journeyofbabo@gmail.com>
2024-05-30 15:35:04 +0900

    module system

---
+++ Monguard_Assemble/Monguard_Assemble.ino
@@ -0,0 +1,17 @@
+#include "MonguardLib.h"
+
+// Correct object declaration
+Monguard myMonguard(13);
+
+void setup() {
+  // Setup the pin mode
+  myMonguard.begin();
+}
+
+void loop() {
+  // Use the Monguard object to send Morse code
+  myMonguard.dot(); myMonguard.dot(); myMonguard.dot();
+  myMonguard.dash(); myMonguard.dash(); myMonguard.dash();
+  myMonguard.dot(); myMonguard.dot(); myMonguard.dot();
+  delay(3000);
+}
