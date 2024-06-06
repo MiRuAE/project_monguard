0c58c37c13e4099820a4cd33f9bdcc54c80ebfcc
KimMinwoo1214 <werkm1214@hanyang.ac.kr>
2024-06-06 21:37:23 +0900

    Servo_Library_Made

---
+++ Monguard_Assemble/MyServoControl.h
@@ -0,0 +1,30 @@
+#ifndef MyServoControl_h
+#define MyServoControl_h
+
+#include <Arduino.h>
+
+class MyServoControl {
+public:
+  MyServoControl();
+  void begin();
+  void walkForward();
+  void walkBackward();
+  void positionSet();
+  void increaseAngle(uint16_t value);
+  void decreaseAngle(uint16_t value);
+  void tiltLeft(uint16_t value);
+  void tiltRight(uint16_t value);
+  void upDownTilt(uint16_t value);
+
+private:
+  const uint16_t NEUTRAL1 = 3925;
+  const uint16_t NEUTRAL2 = 2375;
+  const uint16_t ANGLE_RANGE = 300;
+  
+  uint16_t currentAngle1;
+  uint16_t currentAngle2;
+
+  void updateServo();
+};
+
+#endif
