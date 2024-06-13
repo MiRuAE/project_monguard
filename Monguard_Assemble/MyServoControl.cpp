1d92f3a7bdfc89b582a1bb3f6e468c6974c700db
shdragron <shdragron@hanyang.ac.kr>
2024-06-13 19:26:41 +0900

    주석 추가

--- Monguard_Assemble/MyServoControl.cpp
+++ Monguard_Assemble/MyServoControl.cpp
@@ -2,34 +2,37 @@
 
 MyServoControl::MyServoControl() : currentAngle1(NEUTRAL1), currentAngle2(NEUTRAL2) {}
 
+
 void MyServoControl::begin() {
   // Serial.begin(9600);
 
-  // Configure Timer/Counter1 for Fast PWM mode
-  cli(); // Disable global interrupts
-  DDRB |= (1 << PB1) | (1 << PB2); // Set OC1A (PB1) and OC1B (PB2) as output
+  // 타이머/카운터1을 Fast PWM 모드로 설정
+  cli(); // 전역 인터럽트 비활성화
+  DDRB |= (1 << PB1) | (1 << PB2);
 
-  // Configure Timer/Counter1
   TCCR1A = (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1); // Fast PWM, 10-bit
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Fast PWM, Prescaler 8
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Fast PWM, 프리스케일러 8
 
-  ICR1 = 40000; // Top value for 50Hz PWM (20ms period)
+  ICR1 = 40000; // 50Hz PWM(20ms 주기)
 
-  sei(); // Enable global interrupts
+  sei(); // 전역 인터럽트 활성화
 
-  // Initialize the PWM values to neutral position
-  OCR1A = NEUTRAL1; // Timer counts in microseconds
-  OCR1B = NEUTRAL2; // Timer counts in microseconds
+  // 중립 위치로 PWM 값을 초기화
+  OCR1A = NEUTRAL1; // 타이머 카운트는 마이크로초 단위
+  OCR1B = NEUTRAL2; // 타이머 카운트는 마이크로초 단위
 }
 
+// 지정된 속도로 위치 설정
 void MyServoControl::positionSet(uint16_t speed) {
   updateServo(NEUTRAL1, NEUTRAL2, speed);
 }
 
+// 앞으로
 void MyServoControl::walkForward(uint16_t speed) {
   updateServo(NEUTRAL1 + ANGLE_RANGE, NEUTRAL2 - ANGLE_RANGE, speed);
 }
 
+// 뒤로
 void MyServoControl::walkBackward(uint16_t speed) {
   updateServo(NEUTRAL1 - ANGLE_RANGE, NEUTRAL2 + ANGLE_RANGE, speed);
 }
@@ -46,39 +49,21 @@ void MyServoControl::decreaseAngle(uint16_t value, uint16_t speed) {
               speed);
 }
 
+// 오른쪽으로 기울기
 void MyServoControl::tiltRight(uint16_t value, uint16_t speed) {
   updateServo(constrain(currentAngle1 + value, NEUTRAL1 - ANGLE_RANGE, NEUTRAL1 + ANGLE_RANGE),
               constrain(currentAngle2 + value, NEUTRAL2 - ANGLE_RANGE, NEUTRAL2 + ANGLE_RANGE),
               speed);
 }
 
+// 왼쪽으로 기울기
 void MyServoControl::tiltLeft(uint16_t value, uint16_t speed) {
   updateServo(constrain(currentAngle1 - value, NEUTRAL1 - ANGLE_RANGE, NEUTRAL1 + ANGLE_RANGE),
               constrain(currentAngle2 - value, NEUTRAL2 - ANGLE_RANGE, NEUTRAL2 + ANGLE_RANGE),
               speed);
 }
 
+// 상하
 void MyServoControl::upDownTilt(uint16_t value, uint16_t speed) {
   updateServo(constrain(currentAngle1 + value, NEUTRAL1 - ANGLE_RANGE, NEUTRAL1 + ANGLE_RANGE),
               constrain(currentAngle2 - value, NEUTRAL2 - ANGLE_RANGE, NEUTRAL2 + ANGLE_RANGE),
-              speed);
-}
-
-void MyServoControl::updateServo(uint16_t targetAngle1, uint16_t targetAngle2, uint16_t speed) {
-  int step1 = (targetAngle1 > currentAngle1) ? 1 : -1;
-  int step2 = (targetAngle2 > currentAngle2) ? 1 : -1;
-
-  while (currentAngle1 != targetAngle1 || currentAngle2 != targetAngle2) {
-    if (currentAngle1 != targetAngle1) {
-      currentAngle1 += step1;
-    }
-    if (currentAngle2 != targetAngle2) {
-      currentAngle2 += step2;
-    }
-
-    OCR1A = currentAngle1;
-    OCR1B = currentAngle2;
-
-    delay(speed);
-  }
-}
